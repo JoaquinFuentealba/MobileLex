9d21a75f0941cdf8c58c600726c2a95bcfdbe99f
Joaquin <jfuentealbarojas@gmail.com>
2018-12-08 00:43:42 -0300

    se sube nuevo consuumo de servcio

--- app/src/main/java/client/lex/com/mobilelex/domain/Constante.java
+++ app/src/main/java/client/lex/com/mobilelex/domain/Constante.java
@@ -1,7 +1,11 @@
 package client.lex.com.mobilelex.domain;
 
 public class Constante {
-    public static String URL = "http://192.168.230.1:7001/";
+
+    //public static String URL = "http://192.168.230.1:7001/";
+    public static String URL="http://40.117.224.51:7001/";
+    //public static String URL = "http://172.20.10.3:7001/";
     public static String SERVICIO_LOGGIIN = URL + "ServiciosLexAndroid/resources/ServiceLexAbogado/loggin";
+    public static String SERVICIO_PRODUCT = URL +"ServiciosLexAndroid/resources/ServiceLexAbogado/getAllPresupuesto";
     public static String  KEY ="<add key=\"Encryption\" value=\"AKFG30928agPGXH20GTYBB010200EHEXA\"/>";
 }
